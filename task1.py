caee452a346a63f6baadba73bbe33638ab61a69c
redcommander561 <jaysona803@deltalearns.ca>
2025-01-21 10:41:59 -0800

    .
    .

--- task1.py
+++ task1.py
@@ -33,7 +33,6 @@ class quadratic:
         # defined when the discriminant is non negative
         # return value should be True or False
         
-
     def isFactorable(self):
         x = [1,4,9,16,49,64,81,100,]
         if self.discriminant() in x:
@@ -49,8 +48,8 @@ class quadratic:
         # quadratic can be factored if the discriminant is a perfect square
         # return value is True or False
         
-        
     def calcRoots(self):
+        #google to the rescue
         if not self.hasRealRoots():
             self.roots = []
             return self.roots
@@ -62,6 +61,7 @@ class quadratic:
             root1 = (-self.b + math.sqrt(d)) / (2 * self.a)
             root2 = (-self.b - math.sqrt(d)) / (2 * self.a)
         self.roots = sorted([round(root1, 2), round(root2, 2)])
+        print(self.roots)
         return self.roots
         # requires no positional arguments
         # will make use of class properties a,b and c
@@ -77,21 +77,24 @@ class quadratic:
         # roots should be rounded to 2 decimal places
 
     def axisOfSymmetry(self):
+        what = -self.b / (2 * self.a)
+        return what
         # requires no positional arguments
         # will make use of class properties a,b and c
         # to determine the x value that is for the equation
         # of the axis of symmetry
         # should return the x value for the axis of symmetry
-        pass
-        return
+       
 
     def vertex(self):
+        x_vertex = -self.b / (2 * self.a)
+        y_vertex = self.a * x_vertex**2 + self.b * x_vertex + self.c
+        return [round(x_vertex, 2), round(y_vertex, 2)]
         # requires no positional arguments
         # will make use of class properties a,b and c
         # to determine the x,y value of the vertex
         # should return the a list with the x and y coordinates of the vertex
-        pass
-        return
+        
        
     def __init__(self,a,b,c):
         self.a = a
@@ -103,32 +106,37 @@ class quadratic:
 
 
 if __name__ == "__main__":
-    q1 = quadratic(1,4,4)
+    q1 = quadratic(1, 4, 4)
+    q1.calcRoots() 
     assert q1.isFactorable() == True
     assert q1.hasRealRoots() == True
     assert q1.discriminant() == 0
-    assert q1.roots == [-2,-2]
-    assert q1.axisOfSymmetry == -2
-    assert q1.vertex == [-2,0]
+    assert q1.roots == [-2, -2]
+    assert q1.axisOfSymmetry() == -2
+    assert q1.vertex() == [-2, 0]
 
-    q2 = quadratic(1,1,-6)
+    q2 = quadratic(1, 1, -6)
+    q2.calcRoots()  
     assert q2.isFactorable() == True
     assert q2.hasRealRoots() == True
     assert q2.discriminant() == 25
-    assert q2.roots == [-3,2]
-    assert q2.axisOfSymmetry == -0.5
-    assert q2.vertex == [-0.5,-6.25]
+    assert q2.roots == [-3, 2]
+    assert q2.axisOfSymmetry() == -0.5
+    assert q2.vertex() == [-0.5, -6.25]
 
-    q3 = quadratic(1,1,10)
+    q3 = quadratic(1, 1, 10)
+    q3.calcRoots() 
     assert q3.isFactorable() == False
     assert q3.hasRealRoots() == False
     assert q3.discriminant() == -39
     assert q3.roots == []
-    assert q3.axisOfSymmetry == -0.5
+    assert q3.axisOfSymmetry() == -0.5
 
-    q4 = quadratic(1,10,1)
+    q4 = quadratic(1, 10, 1)
+    q4.calcRoots()  
     assert q4.isFactorable() == False
     assert q4.hasRealRoots() == True
     assert q4.discriminant() == 96
-    assert q4.roots == [-9.90,-0.10]
-    assert q4.axisOfSymmetry == -2.5
+    assert q4.roots == [-9.9, -0.1]
+    assert q4.axisOfSymmetry() == -5
+    assert q4.vertex() == [-5, 24]
--- testtest.py
+++ testtest.py
@@ -1,3 +1,4 @@
+#assertation error with line 69 roots 
 import math
 
 class quadratic:
@@ -11,16 +12,26 @@ class quadratic:
         return self.b**2 - 4*self.a*self.c
 
     def hasRealRoots(self):
-        
-        return self.discriminant() >= 0
+        if self.discriminant() > 0:
+            print("Two roots")
+            return True
+        elif self.discriminant() == 0:
+            print("One root")
+            return True
+        else:
+            print("No real roots")
+            return False
 
     def isFactorable(self):
-        
-        d = self.discriminant()
-        return d >= 0 and math.isqrt(d)**2 == d
+        x = [1, 4, 9, 16, 49, 64, 81, 100]
+        if self.discriminant() in x:
+            return True
+        elif self.discriminant() >= 0 and math.isqrt(self.discriminant())**2 == self.discriminant():
+            return True
+        else:
+            return False
 
     def calcRoots(self):
-        
         if not self.hasRealRoots():
             self.roots = []
             return self.roots
@@ -31,22 +42,28 @@ class quadratic:
         else:
             root1 = (-self.b + math.sqrt(d)) / (2 * self.a)
             root2 = (-self.b - math.sqrt(d)) / (2 * self.a)
-
-        
         self.roots = sorted([round(root1, 2), round(root2, 2)])
+        print(self.roots)
         return self.roots
 
-    
+    def axisOfSymmetry(self):
+        what = -self.b / (2 * self.a)
+        return what
 
-    
+    def vertex(self):
+        x_vertex = -self.b / (2 * self.a)
+        y_vertex = self.a * x_vertex**2 + self.b * x_vertex + self.c
+        return [round(x_vertex, 2), round(y_vertex, 2)]
 
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
 
+
 if __name__ == "__main__":
     q1 = quadratic(1, 4, 4)
+    q1.calcRoots() 
     assert q1.isFactorable() == True
     assert q1.hasRealRoots() == True
     assert q1.discriminant() == 0
@@ -55,6 +72,7 @@ if __name__ == "__main__":
     assert q1.vertex() == [-2, 0]
 
     q2 = quadratic(1, 1, -6)
+    q2.calcRoots()  
     assert q2.isFactorable() == True
     assert q2.hasRealRoots() == True
     assert q2.discriminant() == 25
@@ -63,6 +81,7 @@ if __name__ == "__main__":
     assert q2.vertex() == [-0.5, -6.25]
 
     q3 = quadratic(1, 1, 10)
+    q3.calcRoots() 
     assert q3.isFactorable() == False
     assert q3.hasRealRoots() == False
     assert q3.discriminant() == -39
@@ -70,9 +89,10 @@ if __name__ == "__main__":
     assert q3.axisOfSymmetry() == -0.5
 
     q4 = quadratic(1, 10, 1)
+    q4.calcRoots()  
     assert q4.isFactorable() == False
     assert q4.hasRealRoots() == True
     assert q4.discriminant() == 96
     assert q4.roots == [-9.9, -0.1]
     assert q4.axisOfSymmetry() == -5
-    assert q4.vertex() == [-5, 26]
+    assert q4.vertex() == [-5, 24]
